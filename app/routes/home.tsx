784d9b1bf0348006b239d3303d36339ffa9c7f1c
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-07-31 15:30:29 -0300

    Refactor code structure for improved readability and maintainability

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'Resumind' },
+    { name: 'description', content: 'Smart feedback for your dream job!' },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className='bg-[url(/images/bg-main.svg)] bg-cover bg-center bg-no-repeat'>
+      <section className='main-section'>
+        <div className='page-heading'>
+          <h1 className='text-4xl font-bold'>Resumind</h1>
+          <h2 className='text-lg mt-2'>
+            Revisa tus postulaciones y accede a comentarios generados por IA
+          </h2>
+        </div>
+      </section>
+    </main>
+  );
 }
